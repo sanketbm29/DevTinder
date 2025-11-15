50fbb2087bf74d84b71f67ef7ffa8337406b415a
Sanket <sanketbm.007@gmail.com>
2025-11-15 22:36:24 +0530

    exploring get, post, delete

--- src/app.js
+++ src/app.js
@@ -1,8 +1,18 @@
 const express = require("express");
 
 const app = express();
+// app.use(express.json());
 
-app.use(express.json());
+app.get("/user", (req, res) =>{
+  res.send({firstname :"Sanket", lastname:"Memane"});
+})
+app.post("/user", (req, res) =>{
+  console.log(req.body);
+  res.send("Data successfully saved to DB");
+})
+app.delete("/user", (req, res) =>{
+  res.send("Deleted successfully");
+})
 
 const port = 3000;
 
