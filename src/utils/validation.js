50f9018926db0ad6df6f8b1c845e1afd723ad81e
Sanket <sanketbm.007@gmail.com>
2025-12-27 21:25:42 +0530

    logout & edit api

--- src/utils/validation.js
+++ src/utils/validation.js
@@ -12,4 +12,23 @@ const validateSignupData = (req) => {
   }
 };
 
-module.exports = { validateSignupData };
+const validateEditProfileData = (req) => {
+  const allowedEditFields = [
+    "firstName",
+    "lastName",
+    "emailId",
+    "age",
+    "photoUrl",
+    "gender",
+    "about",
+    "skills",
+  ];
+
+  const isEditAllowed = Object.keys(req.body).every((field) =>
+    allowedEditFields.includes(field)
+  );
+
+  return isEditAllowed;
+};
+
+module.exports = { validateSignupData, validateEditProfileData };
