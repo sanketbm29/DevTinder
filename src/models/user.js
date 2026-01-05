f724d2d4e01409f2cc37e71d396afbd4ffc9e48f
Sanket <sanketbm.007@gmail.com>
2026-01-05 23:46:06 +0530

    Queries & indexes in DB & Handle corner cases

--- src/models/user.js
+++ src/models/user.js
@@ -41,11 +41,16 @@ const userSchema = new mongoose.Schema(
     },
     gender: {
       type: String,
-      validate(value) {
-        if (!["male", "female", "others"].includes(value)) {
-          throw new Error("Gender data not valid");
-        }
+      enum: {
+        values: ["male", "female", "others"],
+        message: `{VALUE} is not a valid gender type`,
       },
+
+      // validate(value) {
+      //   if (!["male", "female", "others"].includes(value)) {
+      //     throw new Error("Gender data not valid");
+      //   }
+      // },
     },
     photoUrl: {
       type: String,
