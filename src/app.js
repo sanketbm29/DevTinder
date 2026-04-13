d3e13c97bfe67b24075c3e050cc9b743d3d1803c
Sanket <sanketbm.007@gmail.com>
2026-04-13 16:09:46 +0530

    cors & response added

--- src/app.js
+++ src/app.js
@@ -2,9 +2,16 @@ const express = require("express");
 const connectDB = require("./config/database");
 const app = express();
 const cookieParser = require("cookie-parser");
+const cors = require("cors");
 
 app.use(express.json());
 app.use(cookieParser());
+app.use(
+  cors({
+    origin: "http://localhost:5173",
+    credentials: true,
+  }),
+);
 
 const authRouter = require("./routes//auth");
 const profileRouter = require("./routes/profile");
